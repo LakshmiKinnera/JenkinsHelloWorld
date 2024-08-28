d52586a1482931ad7c3ec5d0a1d8291981964e2c
LakshmiKinnera <78419973+LakshmiKinnera@users.noreply.github.com>
2024-08-28 14:17:34 +0530

    Update HelloWorld.java
    EDITED THE COMMENT

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("-----***** Hello World!! See you soon *****-----");
+		System.out.println("-----***** Hello World!! See you soon AT 9pm*****-----");
 	}
 }
